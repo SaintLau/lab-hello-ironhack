d8285e125e8f0ce9cd4145a5406dc39a383a1b43
Laura Santos <alsr16p@gmail.com>
2021-02-22 18:06:18 +0000

    fixed some errors, still need to add styles

--- src/components/About.js
+++ src/components/About.js
@@ -14,40 +14,47 @@ function About() {
                 </div>
             </div>
 
+    <div className="wrapper">
+        <div className="container">
+            <div className="row global-row">
+
+
             <div className="col-sm-3 column-sizer">
                 <div className="icon1">
-                    <img src="public/images/icon1.png" />
+                    <img src="public/images/icon1.png" className="icons"/>
                     <h3><b>Declarative</b></h3>
                     <p>React makes it painless to create interactive UIs</p>
                 </div>
 
                  <div className="col-sm-3 column-sizer">
-                    <img src="public/images/icon2.png" />
+                    <img src="public/images/icon2.png" className="icons"/>
                     <h3><b>Components</b></h3>
                     <p>Build encapsulated components that manage their state</p>
 
                  </div>
 
                  <div className="col-sm-3 column-sizer">
-                    <img src="public/images/icon3.png" />
+                    <img src="public/images/icon3.png" className="icons"/>
                     <h3><b>Single-Way</b></h3>
                     <p>A set of immutable values are passed to the component's</p>
 
                  </div>
 
                  <div className="col-sm-3 column-sizer">
-                    <img src="public/images/icon4.png" />
-                    <h3><b>Statically-typed, designed to run on modern browsers.</b></h3>
+                    <img src="public/images/icon4.png" className="icons"/>
+                    <h3><b>JSX</b></h3>
+                    <p>Statically-typed, designed to run on modern browsers.</p>
                  </div>
 
             </div>
 
-               
-
-
+         </div>
+       </div>           
     </div>
+
+  </div>
         
-    )
+   )
 }
 
 export default About;
